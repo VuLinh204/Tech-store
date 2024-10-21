75c53d107732405fa0d55bc925e06e4777ca2378
Thành Đạt <honguyenthanhdat01012003@gmail.com>
2024-10-21 22:20:38 +0700

    update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Banner from './components/Banner';
-import Content from './components/Content';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Content from "./components/Content";
 // import ProductList from './components/ProductList';
-import './App.css';
-import './Base.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./App.css";
+import "./Base.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Banner />
-            <Content />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ViewCart from "./components/ViewCart";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ViewCart" element={<ViewCart />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);  
 
 reportWebVitals();
