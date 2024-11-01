e4c82d7c082e1ebe5437103fd76af3b6866a72e2
tuannhat24 <tuannhat124@gmail.com>
2024-11-01 23:10:46 +0700

    .

--- backend/views/getCategories.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('../controllers/CategoriesController.php');
-
-$categoryController = new CategoryController();
-$categories = $categoryController->getAllCategories();
-
-header('Content-Type: application/json');
-
-echo json_encode($categories);
